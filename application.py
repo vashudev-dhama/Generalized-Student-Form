2def587e3ab0aba74a6a5716b47fc766ce38520c
vashudev-dhama <dhamavashu1119@gmail.com>
2019-10-01 00:48:21 +0530

    csv updation and email sending

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-# import os
-# import smtplib
+import os
+import smtplib
 from flask import Flask, render_template, request, redirect
 import csv
 
@@ -12,19 +12,21 @@ students = []
 def index():
     return render_template("index.html")
 
-# @app.route("/registrants")
-# def registrants():
-#     return render_template("registered.html", students = students)
-
-#listen for '/register'
 @app.route("/register", methods=["POST"])
 def register():
     if not request.form.get("name") or not request.form.get("branch") or not request.form.get("email"):
-        return "failure"
+        return render_template("failure.html")
+    #write in csv file
     file = open("registered.csv", "a") # a for append mode
     writer = csv.writer(file)
-    writer.writerow((request.form.get("name"), request.form.get("branch"))) #extra braces for tuple
+    writer.writerow((request.form.get("name"), request.form.get("branch"), request.form.get("email"))) #extra braces for tuple
     file.close()
+    #send confirmation mail
+    email_msg = f"Hello {request.form.get('name')}, your response has been recorded. We're working on your payment issues and hope to resolve ASAP. Till then just have some patience and keep doing your good work."
+    server = smtplib.SMTP("smtp.gmail.com", 587) #server to be used for mailing
+    server.starttls() #encryption
+    server.login("vashudev.1613121@kiet.edu", "itsforkietidOk?") #sender's credentials
+    server.sendmail("vashudev.1613121@kiet.edu", request.form.get("email"), email_msg) #from-to with msg
     return render_template("success.html", name = request.form.get("name"))
 
 @app.route("/registered")
